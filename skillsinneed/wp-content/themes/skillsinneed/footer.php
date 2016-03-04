73d57b908fe495abe5cc0e953dcc32697615199d
Sean Cho <seancho0420@gmail.com>
2016-03-03 20:47:18 -0500

    Added header and footer

--- skillsinneed/wp-content/themes/skillsinneed/footer.php
+++ skillsinneed/wp-content/themes/skillsinneed/footer.php
@@ -178,5 +178,6 @@
 		<!-- Custom Scripts -->
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/custom.js"></script>
 
+		<?php wp_footer(); ?>
 	</body>
 </html>
