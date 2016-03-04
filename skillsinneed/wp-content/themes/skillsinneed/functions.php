6866f6bd9299aa86e62c4713010df7fb0e9fe6d8
Sean Cho <sean@Seans-MacBook-Pro.local>
2016-03-03 21:09:37 -0500

    Added menu to appearance

--- skillsinneed/wp-content/themes/skillsinneed/functions.php
+++ skillsinneed/wp-content/themes/skillsinneed/functions.php
@@ -1,4 +1,60 @@
 <?php
+if ( ! function_exists( 'skillsinneed_setup' ) ) :
+
+function skillsinneed_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on twentyfifteen, use a find and replace
+	 * to change 'twentyfifteen' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'skillsinneed', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 825, 510, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu',      'skillsinneed' ),
+		'social'  => __( 'Social Links Menu', 'skillsinneed' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	) );
+}
+endif; // skillsinneed_setup
+add_action( 'after_setup_theme', 'skillsinneed_setup' );
+
 
 add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
 
